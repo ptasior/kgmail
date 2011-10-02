c458ac4218cf6c5a5bb8053eabb008e90f20b22e
Maciek <ptasior3@gazeta.pl>
2011-10-02 21:34:09 +0200

    Almost working - authorisation wnd issues at first run

--- kgmail.pl
+++ kgmail.pl
@@ -20,7 +20,9 @@ sub new
 				_wnd => 0,
 				_processing => 0,
 				_user => '',
-				_pwd => ''
+				_pwd => '',
+				_authWnd => '',
+				_mailCnt => 0
 			};
 		$instance = bless $self, $class;
 	}
@@ -32,8 +34,8 @@ sub init
 	my ($self) = @_;
 	my $w = Wnd->new;
 	$self->{_wnd} = $w;
-	die 'no auth data' if ! Auth->getData;
-	$w->timerEvent;
+	
+	$w->setup()if(!Auth->getData());
 }
 
 sub wnd
@@ -61,6 +63,20 @@ sub pwd
 	my ($self, $v) = @_;
 	$self->{_pwd} = $v if defined($v);
 	return $self->{_pwd};
+}
+
+sub authWnd
+{
+	my ($self, $v) = @_;
+	$self->{_authWnd} = $v if defined($v);
+	return $self->{_authWnd};
+}
+
+sub mailCnt
+{
+	my ($self, $v) = @_;
+	$self->{_mailCnt} = $v if defined($v);
+	return $self->{_mailCnt};
 }
 
 ###########################################################################################
@@ -115,29 +131,31 @@ sub check
 	return if($mapp->processing() == 1);
 	$mapp->processing(1);
 	
+	return if !defined($mapp->user()) || $mapp->user() eq '';
+	
 	my $socket = IO::Socket::SSL->new(
 			PeerAddr => 'imap.gmail.com',
 			PeerPort => 993,
 		)
-		or die "socket(): $@";
+		or die "Can't create connection - socket(): $@";
 
 	my $client = Mail::IMAPClient->new(
 			Socket   => $socket,
 			User     => $mapp->user,
 			Password => $mapp->pwd,
 		)
-		or die "new(): $@";
+		or die "Can't connect - new(): $@";
 
 	$client->select('INBOX');
 
-# 	print "connected\n";
-	
+	print "Checking\n";
+
 	my @unread = $client->unseen;
 	$self->{_cnt} = $client->unseen_count;
+	$mapp->mailCnt($self->{_cnt});
 	
 	if($self->{_cnt} != 0)
 	{
-# 		print 'unread: '.join(', ', @unread);
 		foreach my $msg (@unread)
 		{
 			push @{$self->{_subject}}, $client->subject($msg) or die "Couldn't get message subject\n";
@@ -157,9 +175,11 @@ package Wnd;
 
 use QtCore4;
 use QtGui4;
-use QtCore4::isa qw(Qt::TextBrowser);
+use QtCore4::isa qw(Qt::TextEdit);
 use QtCore4::slots
 		timerEvent => [],
+		setup => [],
+		check => [],
 		showHide => ['QSystemTrayIcon::ActivationReason'];
 		
 use QtCore4::debug qw(ambiguous);
@@ -176,17 +196,25 @@ sub new
 	this->connect($timer, SIGNAL 'timeout()', this, SLOT 'timerEvent()');
 	this->connect(this, SIGNAL 'selectionChanged()', this, SLOT 'hide()');
 	$timer->start(100000);
+	Qt::Timer::singleShot(1000, this, SLOT 'timerEvent()');
 	
 	my $menu = Qt::Menu;
-	my $acQuit= Qt::Action(Qt::Icon('icon.png'), 'Quit', $menu);
-	my $trayIcon = Qt::SystemTrayIcon(Qt::Icon('icon.png'));
+	my $acCheck = Qt::Action('Check now!', $menu);
+	my $acSetup = Qt::Action('Setup', $menu);
+	my $acQuit = Qt::Action('Quit', $menu);
+	my $trayIcon = Qt::SystemTrayIcon(Qt::Icon('iconwait.png'));
 	
+	this->connect($acCheck, SIGNAL 'triggered()', this, SLOT 'check()');
+	this->connect($acSetup, SIGNAL 'triggered()', this, SLOT 'setup()');
 	this->connect($acQuit, SIGNAL 'triggered()', qApp, SLOT 'quit()');
 	this->connect($trayIcon, SIGNAL 'activated(QSystemTrayIcon::ActivationReason)', this, SLOT 'showHide(QSystemTrayIcon::ActivationReason)');
 	
+	$menu->addAction($acCheck);
+	$menu->addAction($acSetup);
 	$menu->addAction($acQuit);
 	$trayIcon->setContextMenu($menu);
 	
+	$trayIcon->setToolTip('Initialising...');
 	$trayIcon->show;
 	
 	my $self = {
@@ -206,18 +234,31 @@ sub tray
 
 sub timerEvent
 {
+	my $mapp = MApp->new;
+	$mapp->wnd->tray->setToolTip('Checking mails...');
+	
+	die 'No credentials' if !Auth->getData();
+	
 	my $im = new Imap;
 	$im->check;
-	my $mapp = MApp->new;
 	$mapp->wnd->tray->setToolTip('Unread: '.$im->cnt());
 
-	my $txt;
-	for(my $i = 0; $i < $im->cnt(); $i++)
+	my $txt = '';
+	for(my $i = $im->cnt()-1; $i >= 0; $i--)
 	{
 		my $bd = $im->body($i);
 		$bd =~ s/<.*?>//g;
 		$bd = substr($bd,0,70);
 		$txt .= '<b>'.$im->subject($i).'</b> <i style="font-size: 20%">'.$im->author($i).'</i><br>'.$bd.'<br><br><hr>';
+	}
+	
+	if($im->cnt())
+	{
+		$mapp->wnd->tray->setIcon(Qt::Icon('icon.png'));
+	}
+	else
+	{
+		$mapp->wnd->tray->setIcon(Qt::Icon('iconno.png'));
 	}
 
 	setText($txt);
@@ -226,6 +267,20 @@ sub timerEvent
 sub showHide
 {
 	setVisible(isHidden()) if($_[0] == 3);
+	my $mapp = MApp->new;
+	setVisible(0) if($mapp->mailCnt == 0);
+}
+
+sub check
+{
+	timerEvent();
+}
+
+sub setup
+{
+	my $mapp = MApp->new;
+	my $aw = Auth->new;
+	$mapp->authWnd($aw);
 }
 
 ###########################################################################################
@@ -236,8 +291,8 @@ use QtCore4;
 use QtGui4;
 use QtCore4::isa qw(Qt::Widget);
 use QtCore4::slots
-		timerEvent => [],
-		showHide => ['QSystemTrayIcon::ActivationReason'];
+		save => [],
+		closeWnd => [];
 		
 use QtCore4::debug qw(ambiguous);
 
@@ -246,9 +301,43 @@ sub new
 	my $class = shift;
 	$class->SUPER::NEW(@_);
 	
-	my $self = {};
+	my $mapp = MApp->new;
 
+	$mapp->authWnd(this);
+
+	resize(200, 100);
+	
+	my $ok = Qt::PushButton('OK');
+	my $cancel = Qt::PushButton('Cancel');
+	my $login = Qt::LineEdit($mapp->user);
+	my $pwd = Qt::LineEdit($mapp->pwd);
+	$pwd->setEchoMode(2);
+
+	my $mainLayout = Qt::VBoxLayout();
+
+	$mainLayout->addWidget(Qt::Label('Login:'));
+	$mainLayout->addWidget($login);
+	$mainLayout->addWidget(Qt::Label('Password:'));
+	$mainLayout->addWidget($pwd);
+	
+	my $barLayout = Qt::HBoxLayout();
+	
+	$barLayout->addWidget($ok);
+	$barLayout->addWidget($cancel);
+	
+	$mainLayout->addLayout($barLayout);
+	this->setLayout($mainLayout);
+	
+	this->connect($ok, SIGNAL 'clicked()', this, SLOT 'save()');
+	this->connect($cancel, SIGNAL 'clicked()', this, SLOT 'closeWnd()');
+
+	my $self = {
+			_login => $login,
+			_pwd => $pwd
+		};
 	bless $self, $class;
+	
+	show();
 	return $self;
 }
 
@@ -265,12 +354,8 @@ sub getData
 	
 	if($KWallet->hasFolder($kwallet_handle, $app_name, $app_name))
 	{
-		$KWallet->createFolder($kwallet_handle, $app_name, $app_name);
-		# $KWallet->writePassword($kwallet_handle, $app_name, 'Password', 'qq', $app_name);
 		my $u = $KWallet->readPassword($kwallet_handle, $app_name, 'User', $app_name);
 		my $p = $KWallet->readPassword($kwallet_handle, $app_name, 'Password', $app_name);
-		print "User ID  = $u\n";
-		print "Password = $p\n";
 		$mapp->user($u);
 		$mapp->pwd($p);
 		
@@ -278,6 +363,38 @@ sub getData
 	}
 	
 	return 0;
+}
+
+sub save
+{
+	my $mapp = MApp->new;
+	
+	my $app_name = 'kgmail';
+	my $bus = Net::DBus->find() or die "Can't find DBus";
+	my $kwallet_service = $bus->get_service('org.kde.kwalletd') or die "Can't get kwallet";
+
+	my $KWallet = $kwallet_service->get_object('/modules/kwalletd', 'org.kde.KWallet') or die "Can't find networkWallet";
+	my $kwallet_handle = $KWallet->open($KWallet->networkWallet(), 0, $app_name);
+	
+	if($KWallet->hasFolder($kwallet_handle, $app_name, $app_name))
+	{
+		$KWallet->createFolder($kwallet_handle, $app_name, $app_name);
+	}
+	
+	$KWallet->writePassword($kwallet_handle, $app_name, 'User', $mapp->authWnd->{_login}->text, $app_name);
+	$KWallet->writePassword($kwallet_handle, $app_name, 'Password', $mapp->authWnd->{_pwd}->text, $app_name);
+	$mapp->user($mapp->authWnd->{_login}->text);
+	$mapp->pwd($mapp->authWnd->{_pwd}->text);
+	
+	$mapp->wnd->check();
+	hide();
+}
+
+sub closeWnd
+{
+	hide();
+	my $mapp = MApp->new;
+	$mapp->wnd->check();
 }
 
 ###########################################################################################
@@ -289,9 +406,6 @@ my $a = Qt::Application(\@ARGV);
 
 my $mapp = MApp->new;
 $mapp->init;
-
-# my $aw = Auth->new;
-# $aw->setVisible(1);
 
 exit $a->exec;
 
